faa00024a8ac63139435eb78e6e67fbf352301c4
Thiago Santos <playsd395@gmail.com>
2024-11-15 10:32:22 -0300

    new: Exploração de vizinhança

--- a_star.py
+++ a_star.py
@@ -9,32 +9,52 @@ def a_star(maze_map, initial_position, end_position):
     open_list = []
     closed_list = []
 
-    open_list.append((initial_position, 0))
+    # [posicao-inicial, custo-ate-fim, custo-percorrido, pai] - se pai = 0, não tem pai
+    open_list.append((initial_position, 0, 0, 0))
 
     # while True:
     if not open_list:
         print("No path was found")
     else:
-        position = open_list[-1][0]
-        neighborhood = [neighbor for neighbor, value in maze_map[position].items() if value == 1]
-        print(neighborhood)
+        position = open_list[-1]
+        
 
 
+
+
+# Recebe um posição atual e um movimento e retorna a nova posição
 def new_position(position, move):
     if move == 'E':
-        position[1] += 1
-        return position
+        next_position = (position[0], position[1]+1)
+        return next_position
     elif move == 'W':
-        position[1] -= 1
-        return position
+        next_position = (position[0], position[1]-1)
+        return next_position
     elif move == 'N':
-        position[0] -= 1
-        return position
+        next_position = (position[0]-1, position[1])
+        return next_position
     elif move == 'S':
-        position[0] += 1
-        return position
+        next_position = (position[0]+1, position[1])
+        return next_position
     else:
         print("Position not found")
 
+# Calcula o custo da posição inicial até a posição final
 def calculate_cost_destiny(position, end_position):
     return abs((position[0] - end_position[0]) + (position[1] - end_position[1]))
+
+# [mapa, posição, posição-final, lista-fechada] - explora o mapa com base na posição;
+# explora seus vizinhos calculando o custo de cada um; 
+# verifica se existe na lista fechada e retorna ordenando por custo
+def explore_neighborhood(maze_map, position, end_position, closed_list):
+    neighborhood = [neighbor for neighbor, value in maze_map[position].items() if value == 1]
+    neighbors = []
+    for neighbor in neighborhood:
+        position_finded = new_position(position, neighbor)
+        # TODO adicionar verificação e arrumar posição - List Comprehension
+        if position_finded not in closed_list:
+            neighbors.append([position_finded, 
+                              calculate_cost_destiny(position_finded, end_position),
+                              position[1] + 1, position[0]])
+            
+    return neighbors
--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from pyamaze import maze, agent
+from a_star import a_star
 
-labirinto = maze()
+labirinto = maze(5, 15)
 
-labirinto.CreateMaze()
+end_position = (5, 5)
+
+labirinto.CreateMaze(y=end_position[0], x=end_position[1])
 
 # celulas = labirinto.grid
 # print(celulas)
 
-# mapa = labirinto.maze_map
-# print(mapa)
+mapa = labirinto.maze_map
+#print(mapa)
 
 # caminho = labirinto.path
 # print(caminho)
 
-# agente = agent(labirinto, filled=True, footprints=True)
+agente = agent(labirinto, filled=True, footprints=True)
 
-# posicao = agente.position
-# print(posicao)
+posicao = agente.position
+print(posicao)
 # agente.position = (10, 9)
 
 # caminho = {(10, 10): (10, 9), (10, 9): (10, 8), (10, 8): (10, 7)}
 # labirinto.tracePath({agente: caminho}, delay=700)
+
+a_star(mapa, posicao, end_position)
 
 labirinto.run()
